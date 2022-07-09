b559c740b43992f1772b174a5c865389d4f109a8
nameguess-game <94924743+nameguess-game@users.noreply.github.com>
2022-07-09 06:45:40 +0530

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,43 @@
+let wordList = [
+  {
+    word: "anushka",
+    hint: " Never talk much in class and her smile is awesome also precious for someone (only muskan miss knows)",
+  },
+  {
+    word: "sahiba",
+    hint: " Don't have fear of anyone and at the forefront of the battle",
+  },
+  {
+    word: "sahista",
+    hint: " do weird thing's with friend in classroom",
+  },
+  {
+    word: "suhani",
+    hint: " silent and intelligent",
+  },
+  {
+    word: "anjali",
+    hint: " She talks liitle bit and stay silent ",
+  },
+  {
+    word: "vishal",
+    hint: " Barks too much but dont do anything ( joke my freind)",
+  },
+  {
+    word: "arvind",
+    hint: "Everyone calls him don but sahiba kuch or name rkh di uska",
+  },
+  {
+    word: "sushant",
+    hint: " Lazy or bas chewing gum chabata hai ",
+  },
+  {
+    word: "keshav",
+    hint: " Always in bauraya hua mood",
+  },
+  {
+    word: "shakti",
+    hint: "He takes every serious thing as a joke ",
+  },
+  {},
+];
